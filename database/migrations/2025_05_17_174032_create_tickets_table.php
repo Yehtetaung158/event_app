15157c8f94bdc837cf353f71334db0bb8779644d
Yehtetaung158 <137622241+Yehtetaung158@users.noreply.github.com>
2025-05-18 00:24:07 +0630

    created-table

---
+++ database/migrations/2025_05_17_174032_create_tickets_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('event_id');
+            $table-> integer('amount');
+            $table-> integer('available_quantity');
+            $table-> dateTime('sale_start');
+            $table-> dateTime('sale_end');
+            $table-> dateTime('start_time');
+            $table-> dateTime('end_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
---
+++ database/migrations/2025_05_17_175140_create_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('event_id');
+            $table->foreignId('ticket_id');
+            $table->integer('amount');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('confirm_email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
